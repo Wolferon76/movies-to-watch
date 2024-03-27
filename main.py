65111ff7cb290ee2d248d3b4cc13ddbef3ac3024
Your Name <you@example.com>
2024-03-27 10:18:31 +0200

    fr

--- main.py
+++ main.py
@@ -16,20 +16,47 @@ while True:
     print("5. Dzēst filmu no saraksta")
     print("6. Exit")
     choice = input("Enter your choice: ")
-
+    movies_file = open('movies.json') # opening JSON file
+    movies = json.load(movies_file) # returns JSON object as a dictionary
+    movies_file.close()
     if choice == "1":
         # https://www.w3schools.com/python/python_dictionaries.asp
         # https://www.w3schools.com/python/python_lists_add.asp
-        title = input("Enter movie title: ")
-        rating = input("Enter movie rating: ")
+        
+        Film_title = input("Enter movie title: ")
+        film_rating = int(input("Enter movie rating: "))
+        watch = input("Had you watched this movie?(f = false, t = true): ")
+        if watch == "f":
+            watch = False
+        elif watch == "t":
+            watch = True
+
+
+
+        thisdict = {
+            "title": Film_title,
+            "rating": film_rating,
+            "watched": watch
+            }
+        movies.append(thisdict)
+        print(movies)
+        with open("movies.json", "w") as outfile:
+            json.dump(movies, outfile)
         pass
     elif choice == "2":
         # https://www.w3schools.com/python/python_lists_sort.asp
         # https://www.w3schools.com/python/python_dictionaries_access.asp
+        def sort_rating(movies):
+            return int(movies["rating"])
+        movies.sort(key = sort_rating, reverse = False)
+        print(movies[:11])
         pass
     elif choice == "3":
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/python_dictionaries_access.asp
+        for movie in movies:
+             if movie["watched"] == False:
+                print(movie)
         pass
     elif choice == "4":
         # https://www.w3schools.com/python/python_lists_change.asp
